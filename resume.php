d7786493aa4082e533ba9dd6a231158631f9257c
Andersen Pecorone <andersenmp@gmail.com>
2014-06-01 21:03:22 +0200

    CV update

--- resume.php
+++ resume.php
@@ -7,17 +7,17 @@
 			During my career I have worked on multiple software projects in several operating systems environments, for large and small companies with different core businesses in both the public and private sector.
 		</p>
 		<p>
-			Web solution/Application design and development work with internet technologies, relational databases, and client/server technologies. 
+			Web solution/Application design and development work with internet technologies, relational databases, and client/server technologies.
 		</p>
 		<p>
 			Project leadership experience following established coding conventions and project management methodologies.
 		</p>
 		<p>
-			Speaks Portuguese as native language, English as a second language, intermediate in French, and Spanish is familiar. 
+			Speaks Portuguese as native language, English as a second language, intermediate in French, and Spanish is familiar.
 		</p>
-		
+
 		<hr/>
-			
+
 		<h3>Education</h3>
 		<ul>
 	    	<li>
@@ -36,7 +36,7 @@
 		</ul>
 
 		<hr/>
-		
+
 		<h3>Certifications</h3>
 		<ul>
 	    	<li>Professional Scrum Master I by Scrum.org (2013)</li>
@@ -47,7 +47,14 @@
 	    	<li>Advanced ColdFusion MX 7 Developer Adobe (2008)<li>
 	    	<li>Active Server Pages by Brainbench (2008)</li>
 	    </ul>
-	    
+	<hr/>
+
+	<h3>Organizations</h3>
+	<ul>
+			<li><a href="http://www.computer.org/" target="new">IEEE Computer Society</a></li>
+
+		</ul>
+
 	    <hr/>
 	    <h3>Work Experience</h3>
 	    <ul>
@@ -66,38 +73,38 @@
 	    		<p>
 	    	</li>
 	    	<li>
-	    		<p><strong>Senior Systems Analyst / .Net Developer</strong> for 
+	    		<p><strong>Senior Systems Analyst / .Net Developer</strong> for
 	    		<a href="http://www.ncbi.nlm.nih.gov/pmc/articles/PMC2972781/" target="new"> Doctoral Program</a> at
-	    		 <a href="http://www.uminho.pt/" target="new">Universidade do Minho</a> and 
+	    		 <a href="http://www.uminho.pt/" target="new">Universidade do Minho</a> and
 	    		 <a href="http://www.usp.br/" target="new">Universidade de São Paulo</a>
-	    		 <br />Freelancer Consultant 
+	    		 <br />Freelancer Consultant
 	    		 <br /><em>2008</em>
 	    		<p>
 	    	</li>
 	    	<li>
-	    		<p><strong>Senior Systems Analyst / PHP Developer</strong> for 
-	    		 <a href="http://www.humphreyfellowship.org/" target="new">Fellowship Program</a> at 
+	    		<p><strong>Senior Systems Analyst / PHP Developer</strong> for
+	    		 <a href="http://www.humphreyfellowship.org/" target="new">Fellowship Program</a> at
 	    		 <a href="http://www.vcu.edu/" target="new">Virginia Commonwealth University</a>
-	    		 <br />Freelancer Consultant 
+	    		 <br />Freelancer Consultant
 	    		 <br /><em>2007 to 2008</em>
 	    		<p>
 	    	</li>
 	    	<li>
-	    		<p><strong>Senior Systems Analyst</strong> at 
+	    		<p><strong>Senior Systems Analyst</strong> at
 	    		 <a href="http://www.azulseguros.com.br/" target="new">Azul Seguros</a>
 	    		 <br />Employee
 	    		 <br /><em>2005 to 2007</em>
 	    		<p>
 	    	</li>
 	    	<li>
-	    		<p><strong>Senior Systems Analyst</strong> at 
+	    		<p><strong>Senior Systems Analyst</strong> at
 	    		 <a href="http://www.goldencross.com.br/" target="new">Golden Cross</a>
 	    		 <br />Employee
 	    		 <br /><em>2000 to 2005</em>
 	    		<p>
 	    	</li>
 	    	<li>
-	    		<p><strong>Junior Systems Analyst</strong> at 
+	    		<p><strong>Junior Systems Analyst</strong> at
 	    		 <a href="http://www.montreal.com.br/" target="new">Montreal Informática</a>
 	    		 <br />Employee
 	    		 <br /><em>2000</em>
@@ -114,25 +121,25 @@
 				<br />2000 - 2004 - Alcnas Tecnologia Ltda.
 				<br />1999 - 2001 - DDM Consultoria em Informática Ltda.
 	    		<p>
-	    	</li>	
+	    	</li>
  			<li>
-	    		<p><strong>Technical Support</strong> at 
+	    		<p><strong>Technical Support</strong> at
 	    		 <a href="http://www.embratel.com.br/" target="new">EMBRATEL</a>
 	    		 <br />Consultant from Tecnocoop Informatica
 	    		 <br /><em>1998 to 2000</em>
 	    		<p>
 	    	</li>
 	    	<li>
-	    		<p><strong>Technical Support Leader</strong> at 
+	    		<p><strong>Technical Support Leader</strong> at
 	    		 Teclogic Informática
-	    		 <br />Cofounder 
+	    		 <br />Cofounder
 	    		 <br /><em>1996 to 1998</em>
 	    		<p>
 	    	</li>
 	    	<li>
-	    		<p><strong>Trainee</strong> at 
+	    		<p><strong>Trainee</strong> at
 	    		 <a href="http://www.acessa.com/" target="new"> Artnet Internet</a>
-	    		 <br />Trainee 
+	    		 <br />Trainee
 	    		 <br /><em>1996</em>
 	    		<p>
 	    	</li>
@@ -161,7 +168,7 @@
 	</ul>
 </div>
 <div class="five columns clients">
-	<h3>IT Project Lifecycle<br/> (RUP, Agile, Scrum)</h3>
+	<h3>IT Project Lifecycle<br/> (RUP, Agile, Scrum, SWEBOK)</h3>
 	<ul>
 		<li>Requirements Analysis</li>
 		<li>Analysis and Design</li>
@@ -175,24 +182,16 @@
 	<h3>Development</h3>
 	<ul>
 		<li><strong>Analysis and Design:</strong> UML</li>
-		<li><strong>Implementation:</strong> ColdFusion, Fusebox,  ASP, PHP, JavaScript, DOJO, jQuery, VBScript, Visual Basic/VB .Net, 
+		<li><strong>Implementation:</strong> ColdFusion, Fusebox,  ASP, PHP, JavaScript, DOJO, jQuery, VBScript, Visual Basic/VB .Net,
 			HTML, CSS, XML, SQL, C/C++, PL/SQL, T-SQL, Java, and Shell Script.
 		</li>
 		<li><strong>Databases:</strong> Oracle, Microsoft SQL Server, Sybase, MySQL, and PostgreSQL</li>
 		<li><strong>Operation Systems:</strong> Windows, Linux, Mac OS X, and Unix.</li>
 		<li><strong>Source Control:</strong> Microsoft Visual Source Safe(VSS), Subversion(SVN), Git, IBM Rational ClearCase, and SCM IBM Rational Team Concert</li>
-		<li><strong>Tools:</strong> Adobe Dreamweaver, Eclipse, DBDesigner, ERWin, Macromedia HomeSite, Microsoft Project, Microsoft Visual Studio .Net, 
-			TOAD, Sybase PowerDesigner, Oracle Designer, Microsoft Visio, IBM Rational Team Concert, IBM Rational Functional Tester, IBM Rational Quality Manager, 
+		<li><strong>Tools:</strong> Adobe Dreamweaver, Eclipse, DBDesigner, ERWin, Macromedia HomeSite, Microsoft Project, Microsoft Visual Studio .Net,
+			TOAD, Sybase PowerDesigner, Oracle Designer, Microsoft Visio, IBM Rational Team Concert, IBM Rational Functional Tester, IBM Rational Quality Manager,
 			Adobe Acrobat, Adobe LiveCycle, and FTP
 		</li>
 	</ul>
 </div>
 <?php include "footer.php"; ?>
-
-
-
-
-
-
-
- 
